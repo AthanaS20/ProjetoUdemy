aacdbcf92fa6ffb1c7378c74498492cf7255422a
AthanaS20 <106346991+AthanaS20@users.noreply.github.com>
2023-04-26 14:53:28 -0300

    Update factories_methods_classe.py

--- CURSO_PYTHON/factories_methods_classe.py
+++ CURSO_PYTHON/factories_methods_classe.py
@@ -3,3 +3,15 @@
 # ao invés de receber a instância no primeiro
 # parâmetro, receberemos a própria classe.
 
+class Pessoa:
+    def __init__(self, nome, idade):
+        self.nome = nome
+        self.idade = idade
+    
+    @classmethod
+    def idade_50_anos(cls, nome):
+        return cls(nome, 50)
+    
+pessoa_1 = Pessoa('Monica', 30)
+pessoa_2 = Pessoa.idade_50_anos('Helena')
+print(pessoa_2.nome, pessoa_2.idade)
