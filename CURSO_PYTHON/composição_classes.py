25aed609931e8a6bbe95d25f0259289e548bfced
Athana <106346991+AthanaS20@users.noreply.github.com>
2023-05-12 13:19:42 -0300

    add compisão class

--- CURSO_PYTHON/composição_classes.py
+++ CURSO_PYTHON/composição_classes.py
@@ -15,13 +15,23 @@ class Cliente:
     def listar_enderecos(self):
         for enderecos in self.endereco:
             print(enderecos.rua, enderecos.numero)
-            
+    
+    def __del__(self):
+        print('APAGANDO', self.nome)
+    
 class Endereco:
     def __init__(self, rua, numero):
         self.rua = rua
         self.numero = numero
+    
+    def __del__(self):
+        print('APAGANDO', self.rua,self.numero)
 
 cliente1 = Cliente('Renata')
 cliente1.inserir_edereco('Manoel Bandeira', 501)
+cliente1.inserir_edereco('Cocota', 5)
+print(cliente1.nome)
 cliente1.listar_enderecos()
+
+
 
