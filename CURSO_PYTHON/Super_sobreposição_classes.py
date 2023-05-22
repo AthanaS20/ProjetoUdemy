3415230706e1b235bac9764acf6028c50dd24d7b
AthanaS20 <106346991+AthanaS20@users.noreply.github.com>
2023-05-22 19:06:23 -0300

    add new classes

--- CURSO_PYTHON/Super_sobreposição_classes.py
+++ CURSO_PYTHON/Super_sobreposição_classes.py
@@ -14,23 +14,51 @@
 # string = MinhaString('Luiz')
 # print(string.upper())
 
-class A:
+class A(object):
     atributo_a = 'valor a'
+
+    def __init__(self, atributo):
+        self.atributo = atributo
+
     def metodo(self):
         print('A')
 
+
 class B(A):
     atributo_b = 'valor b'
+
+    def __init__(self, atributo, outra_coisa):
+        super().__init__(atributo)
+        self.outra_coisa = outra_coisa
+
     def metodo(self):
         print('B')
 
+
 class C(B):
     atributo_c = 'valor c'
+
+    def __init__(self, *args, **kwargs):
+        # print('EI, burlei o sistema.')
+        super().__init__(*args, **kwargs)
+
     def metodo(self):
-        super().metodo()
+        # super().metodo()  # B
+        # super(B, self).metodo()  # A
+        # super(A, self).metodo()  # object
+        A.metodo(self)
+        B.metodo(self)
         print('C')
 
-c = C()
-print(c.atributo_a)
-print(c.atributo_b)
-print(c.atributo_c)
+
+# print(C.mro())
+# print(B.mro())
+# print(A.mro())
+c = C('Atributo', 'Qualquer')
+# print(c.atributo)
+# print(c.outra_coisa)
+c.metodo()
+# print(c.atributo_a)
+# print(c.atributo_b)
+# print(c.atributo_c)
+# c.metodo()
