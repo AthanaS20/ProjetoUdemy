c0677c109006807a8001c23ef466dbdcd5b19111
AthanaS20 <106346991+AthanaS20@users.noreply.github.com>
2023-05-25 20:28:20 -0300

    update class

--- CURSO_PYTHON/Mixins_abstração_aula_144/log.py
+++ CURSO_PYTHON/Mixins_abstração_aula_144/log.py
@@ -1,5 +1,10 @@
 # Abstração
 
+from pathlib import Path
+
+LOG_FILE = Path(__file__).parent / 'log.txt'
+# Para buscar o caminho desse arquivo (aula_144)
+
 class Log:
     def _log(self, msg):
         raise NotImplementedError('Implemente o método log')
@@ -9,6 +14,7 @@ class Log:
     def log_sucess(self, msg):
         return self._log(f'Sucess: {msg}')
 
+
 class LogFileMixin(Log):
     def _log(self, msg):
         print(msg)
@@ -17,7 +23,19 @@ class LogPrintMixin(Log):
     def _log(self, msg):
         print(f'{msg} {self.__class__.__name__}')
 
+class LogFileMixin(Log):
+    def _log(self, msg):
+        msg_formatada = (f'{msg} {self.__class__.__name__}')
+        print('Salvando no log...')
+        with open (LOG_FILE, 'a') as arquivo: # Criando arquivo log.txt
+            arquivo.write(msg_formatada)
+            arquivo.write('\n')
+
 if __name__ == '__main__':
     l = LogPrintMixin()
     l.log_error('qualquer coisa')
     l.log_sucess('Log realizado com sucesso.')
+    lf = LogFileMixin()
+    lf.log_error('qualquer coisa')
+    lf.log_sucess('Log realizado com sucesso.')
+    
--- CURSO_PYTHON/Mixins_abstração_aula_144/main.py
+++ CURSO_PYTHON/Mixins_abstração_aula_144/main.py
@@ -1,3 +1,8 @@
-from log import Log
+from log import LogFileMixin, LogPrintMixin
 
-print('Oi')
+l = LogPrintMixin()
+l.log_error('qualquer coisa')
+l.log_sucess('Log realizado com sucesso.')
+lf = LogFileMixin()
+lf.log_error('qualquer coisa')
+lf.log_sucess('Log realizado com sucesso.')
