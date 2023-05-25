f8e62f49b9d13a625b621a7ca610ccb77c715854
AthanaS20 <106346991+AthanaS20@users.noreply.github.com>
2023-05-25 20:01:44 -0300

    Update log.py

--- CURSO_PYTHON/Mixins_abstração_aula_144/log.py
+++ CURSO_PYTHON/Mixins_abstração_aula_144/log.py
@@ -1,13 +1,23 @@
 # Abstração
 
 class Log:
-    def log(self, msg):
+    def _log(self, msg):
         raise NotImplementedError('Implemente o método log')
+    
+    def log_error(self, msg):
+        return self._log(f'Error: {msg}')
+    def log_sucess(self, msg):
+        return self._log(f'Sucess: {msg}')
 
 class LogFileMixin(Log):
-    def log(self, msg):
+    def _log(self, msg):
         print(msg)
 
+class LogPrintMixin(Log):
+    def _log(self, msg):
+        print(f'{msg} {self.__class__.__name__}')
+
 if __name__ == '__main__':
-    l = LogFileMixin()
-    l.log('Qualquer coisa')
+    l = LogPrintMixin()
+    l.log_error('qualquer coisa')
+    l.log_sucess('Log realizado com sucesso.')
