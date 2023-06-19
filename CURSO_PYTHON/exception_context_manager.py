263e6f05da80a9b297c53b4976bb47d7829b4383
AthanaS20 <106346991+AthanaS20@users.noreply.github.com>
2023-06-19 11:26:19 -0300

    exceptions in class

---
+++ CURSO_PYTHON/exception_context_manager.py
@@ -0,0 +1,41 @@
+# O método __exit__ receberá a classe de exceção, a exceção e o
+# traceback. Se ele retornar True, exceção no with será
+# suprimidas.
+#
+# Ex:
+# with open('aula149.txt', 'w') as arquivo:
+#     ...
+
+class MyOpen:
+    def __init__(self, caminho_arquivo, modo):
+        self.caminho_arquivo = caminho_arquivo
+        self.modo = modo
+        self._arquivo = None
+
+    #metodo para abrir o arquivo
+    def __enter__(self):
+        print('ABRINDO O ARQUIVO')
+        self._arquivo = open(self.caminho_arquivo, self.modo, encoding='utf8') 
+        return self._arquivo
+
+    # metodo para fechar o arquivo
+    def __exit__(self, class_exception, exception_, traceback_):
+        print('FECHANDO O ARQUIVO')
+        self._arquivo.close()
+
+        # raise class_exception(*exception_.args).with_traceback(traceback_)
+        # print(class_exception)
+        # print(exception_)
+        # print(traceback_)
+
+        exception_.add_note('Mensagem de erro criada por mim.')
+
+        return True # Tratou a exceção, mas dessa forma o erro passa despercebido (Má prática)
+        
+
+# criando o arquivo de texto    
+with MyOpen('context_manage_classe.txt', 'w') as arquivo:
+    arquivo.write('Arquivo 1\n')
+    arquivo.write('Arquivo 2\n')
+    arquivo.write('Arquivo 3\n')
+    print('CRIANDO O ARQUIVO')
