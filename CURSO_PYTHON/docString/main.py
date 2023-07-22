96420702f9ec20e2ac58df5dd389a91c0a97292e
Athana <106346991+AthanaS20@users.noreply.github.com>
2023-07-22 18:02:21 -0300

    docStrings

--- CURSO_PYTHON/docString/main.py
+++ CURSO_PYTHON/docString/main.py
@@ -1,7 +1,7 @@
-import uma_linha
+import varias_linhas
 
 # print(dir(uma_linha)) # Para saber tudo o que o arquivo 'uma_linha' tem
 # print(uma_linha.__name__) # Para saber o nome do arquivo
 # print(uma_linha.__file__) # Caminho do arquivo
 
-print(help(uma_linha)) 
+print(help(varias_linhas)) 
---
+++ CURSO_PYTHON/docString/varias_linhas.py
@@ -0,0 +1,7 @@
+'''
+O que seu modulo faz
+
+(Aqui você pode colocar uma descrição do módulo com várias linhas)
+
+'''
+
