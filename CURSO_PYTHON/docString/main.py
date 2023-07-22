f530aa70cb68c344e631beabb8401bfc4f837d5f
Athana <106346991+AthanaS20@users.noreply.github.com>
2023-07-22 18:11:07 -0300

    continue

--- CURSO_PYTHON/docString/main.py
+++ CURSO_PYTHON/docString/main.py
@@ -1,7 +1,7 @@
-import varias_linhas
+import documentacao_funcao
 
 # print(dir(uma_linha)) # Para saber tudo o que o arquivo 'uma_linha' tem
 # print(uma_linha.__name__) # Para saber o nome do arquivo
 # print(uma_linha.__file__) # Caminho do arquivo
 
-print(help(varias_linhas)) 
+print(help(documentacao_funcao)) 
