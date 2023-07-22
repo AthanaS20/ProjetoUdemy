945c0bbd6247701f7c2a4e29494a3b3193bcf53e
Athana <106346991+AthanaS20@users.noreply.github.com>
2023-07-22 18:20:49 -0300

    docClass

--- CURSO_PYTHON/docString/main.py
+++ CURSO_PYTHON/docString/main.py
@@ -1,7 +1,7 @@
-import documentacao_funcao
+import documentacao_classes
 
 # print(dir(uma_linha)) # Para saber tudo o que o arquivo 'uma_linha' tem
 # print(uma_linha.__name__) # Para saber o nome do arquivo
 # print(uma_linha.__file__) # Caminho do arquivo
 
-print(help(documentacao_funcao)) 
+print(help(documentacao_classes)) 
