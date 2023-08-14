6627ea2d2271833e8194dead1ccd7a71479654d2
Athana <106346991+AthanaS20@users.noreply.github.com>
2023-08-14 16:24:09 -0300

    update e create dataclasses

--- CURSO_PYTHON/data_class.py
+++ CURSO_PYTHON/data_class.py
@@ -16,18 +16,18 @@ class Pessoa:
   def nome_completo(self):
     return f'{self.nome} {self.sobrenome}'
 
-@nome_completo.setter
-    def nome_completo(self, valor):
-        nome, *sobrenome = valor.split()
-        self.nome = nome
-        self.sobrenome = ' '.join(sobrenome)
+  @nome_completo.setter
+  def nome_completo(self, valor):
+    nome, *sobrenome = valor.split()
+    self.nome = nome
+    self.sobrenome = ' '.join(sobrenome)
 
 
 if __name__ == '__main__':
-    p1 = Pessoa('Luiz', 30)
-    p2 = Pessoa('Luiz', 30)
-    print(p1 == p2)
-    p1 = Pessoa('Luiz', 'Otávio')
-    p1.nome_completo = 'Maria Helena'
-    print(p1)
-    print(p1.nome_completo)
+  p1 = Pessoa('Luiz', 30)
+  p2 = Pessoa('Luiz', 30)
+  print(p1 == p2)
+  p1 = Pessoa('Luiz', 'Otávio')
+  p1.nome_completo = 'Maria Helena'
+  print(p1)
+  print(p1.nome_completo)
