2eb90848e3a5bcb521b4c54c16088367572f305b
ATHANA SILVA <athanasilva95@gmail.com>
2023-12-18 20:55:06 -0300

    update

--- CURSO_PYTHON/automatizacao/main.py
+++ CURSO_PYTHON/automatizacao/main.py
@@ -2,6 +2,9 @@ import time
 from pathlib import Path
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 ROOT_FOLDER = Path(__file__).parent
 CHROMEDRIVER_EXC = ROOT_FOLDER / 'drivers' / 'chromedriver.exe'
@@ -36,9 +39,21 @@ def make_chrome_broswer(*options: str) -> webdriver.Chrome:
     return browser
 
 if __name__ == '__main__':
+    TIME_TO_WAIT = 10
     #Ex:
     options = ()
     browser = make_chrome_broswer(*options)
 
+    #Abrir o navegador
     browser.get('https://www.google.com.br/')
-    time.sleep(10)
+
+    # Esperar até que apareça certo elemento
+    search_input = WebDriverWait(browser, TIME_TO_WAIT).until(
+        EC.presence_of_element_located(
+            (By.ID, 'input')
+        )
+    )
+    search_input.send_keys('Hello, world.')
+
+    
+    time.sleep(TIME_TO_WAIT)
