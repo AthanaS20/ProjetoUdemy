6fe7627753d7b70e032a050137d799d583b8b60f
ATHANA SILVA <athanasilva95@gmail.com>
2024-01-12 20:53:36 -0300

    update

--- CURSO_PYTHON/manipulando_imagem/main.py
+++ CURSO_PYTHON/manipulando_imagem/main.py
@@ -5,3 +5,15 @@ ROOT_FOLDER = Path(__file__).parent
 IMAGE_PATH = ROOT_FOLDER / 'foto.jpg'
 NEW_IMAGE = ROOT_FOLDER / 'new.jpg'
 
+pil_image = Image.open(IMAGE_PATH)
+width,height = pil_image.size
+# exif = pil_image.info['exif']
+
+new_width = 640
+new_hight = round(height * new_width / width)
+
+new_size = pil_image.resize((new_hight, new_width))
+new_size.save(
+    NEW_IMAGE,
+    optimize=True,
+)
