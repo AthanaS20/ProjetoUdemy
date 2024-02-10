683b16fda65c6d83c954808ade2874770d998166
ATHANA SILVA <athanasilva95@gmail.com>
2024-02-10 19:26:19 -0300

    update calculadora

--- CURSO_PYTHON/PySide/calculadora/main.py
+++ CURSO_PYTHON/PySide/calculadora/main.py
@@ -1,19 +1,30 @@
 import sys
 from main_window import MainWindow
+from PySide6.QtGui import QIcon
 from PySide6.QtWidgets import (QApplication,
                                QLabel)
+
+from enviroment import WINDOW_ICON_PATH
 
 
 
 
 if __name__ == '__main__':
+
+    #Cria a aplicação
     app = QApplication(sys.argv)
     window = MainWindow()
-
-    label_txt = QLabel('Meu texto')
+    label_txt = QLabel('')
     label_txt.setStyleSheet('font-size: 150px;')
-    window.v_layout.addWidget(label_txt)
+    window.addWidgetToVLayout(label_txt)
     window.adjustFixedSize()
 
+    #Definindo o icone
+    icon = QIcon(str(WINDOW_ICON_PATH))
+    window.setWindowIcon(icon)
+    app.setWindowIcon(icon)
+
+
+    # Executa o programa
     window.show()
     app.exec()
